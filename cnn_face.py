b6881c75d773c912623ca8d587bb1eacf3d5e97e
Rui010 <ito.rui.521@gmail.com>
2018-12-09 13:56:05 +0900

    finish cnn_face

--- cnn_face.py
+++ cnn_face.py
@@ -1,24 +1,26 @@
 import numpy as np
+from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPool2D
 from keras.layers import Dense, Activation, Flatten
 from keras.optimizers import SGD, Adam
+import matplotlib.pyplot as plt
 
 # 乱数シード
-# rng = np.random.RandomState(1234)
+rng = np.random.RandomState(1234)
 random_state = 42
 
 # 学習データ
 X = np.load('./data/x_train.npy')
 t = np.load('./data/t_train.npy')
-# x_train, x_valid, t_train, t_valid = train_test_split(X, t, test_size=0.5, random_state=random_state)
+x_train, x_test, t_train, t_test = train_test_split(X, t, test_size=0.2, random_state=random_state)
+x_train, x_valid, t_train, t_valid = train_test_split(x_train, t_train, test_size=0.1, random_state=random_state)
 
-n_in = len(X[0])
 n_hidden = 200
 n_out = len(t[0])
-print(np.shape(X))
+# print(np.shape(X))S
 
 model = Sequential([
     Conv2D(32,3,input_shape=(64,64,3)),
@@ -36,12 +38,15 @@ model.compile(
     metrics=['accuracy']
 )
 
-model.fit(X, t, epochs=10, batch_size=1)
+hist = model.fit(x_train, t_train, epochs=50,batch_size=10,validation_data=(x_valid, t_valid))
 
-classes = model.predict_classes(X, batch_size=1)
-prob = model.predict_proba(X, batch_size=1)
+val_acc = hist.history['val_acc']
+val_loss = hist.history['val_loss']
 
-print('classified:')
-print(t == classes)
-print('output probability:')
-print(prob)
+fig = plt.figure()
+plt.plot(range(len(val_loss)), val_loss, label='loss')
+plt.xlabel('epochs')
+plt.show()
+
+loss_and_metrics = model.evaluate(x_test, t_test)
+print(loss_and_metrics)
